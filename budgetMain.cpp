b1b40b0b2d2967a9a838d1e1c7749f7646041fd0
Saebyeog <58795546+Saebyeog@users.noreply.github.com>
2022-02-01 00:22:46 -0500

    Create budgetMain.cpp

---
+++ budgetMain.cpp
@@ -0,0 +1,28 @@
+//budgeting system
+//Nicholas
+//31 Jan 2022
+
+#include <iostream>
+
+int main()
+{
+  string = username;
+  
+   std::cout<< "If existing user enter first name" << std::endl;
+   std::cout<< "If new user enter first name" << std::endl;
+   std::cin >> name;
+  
+  //ask if existing user or new user
+    //if existing user enter name(use first name only) or username
+    //else new user put in name or username
+    
+  //ask if deposting, withdrawing, or displaying funds
+  //implement different fund sources and give total sum 
+  //implement reoccuring bills and paychecks
+  //implement gross pay calculator or tax calculator 
+  //be able to save information and reuse it
+  
+  //for personal use only 
+  
+  return 0;
+}
